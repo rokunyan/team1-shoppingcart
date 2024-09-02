07dc0394a26b743907817c381e848ba23aedd020
bpepito <bernadetteotipep@gmail.com>
2024-09-02 14:50:41 +0800

    user edit

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserModule } from './user.module';
+// import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
+// const routes: Routes = [{
+//   path: '',
+//   component: ProfilePageComponent
+// }
+// ];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    // RouterModule.forChild(routes)
   ],
   exports: [
     UserModule
