3e619bab56f75a7ec1ca634548358b4b853aa78e
bpepito <bernadetteotipep@gmail.com>
2024-09-03 15:00:05 +0800

    edit admin-page

--- src/app/modules/admin-page/pages/admin-page-form/admin-page-form.component.ts
+++ src/app/modules/admin-page/pages/admin-page-form/admin-page-form.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { AdminPageService } from '../../services/admin-page.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -8,20 +14,19 @@ import { User } from '../../../user/models/user';
 @Component({
   selector: 'app-admin-page-form',
   templateUrl: './admin-page-form.component.html',
-  styleUrl: './admin-page-form.component.css'
+  styleUrl: './admin-page-form.component.css',
 })
-export class AdminPageFormComponent {
+export class AdminPageFormComponent implements OnDestroy {
   userForm!: FormGroup;
-  userId?: number;
   interestsFormArray!: FormArray;
   sub: Subscription | undefined;
-  
+  passwordLength: number = 8;
+
   constructor(
     private fb: FormBuilder,
     private adminPageService: AdminPageService,
-    private route: ActivatedRoute,
     private router: Router
-  ){
+  ) {
     this.userForm = this.fb.group({
       userName: new FormControl('', Validators.required),
       firstName: new FormControl('', Validators.required),
@@ -29,52 +34,34 @@ export class AdminPageFormComponent {
       lastName: new FormControl('', Validators.required),
       birthDate: new FormControl(''),
       interests: this.fb.array([this.fb.control('')]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-      email: new FormControl('',Validators.required),
-      mobileNumber: new FormControl('', [Validators.minLength(11), Validators.maxLength(11), Validators.pattern('^[0-9]{11}$')]),
+      password: new FormControl(this.generatePassword()),
+      email: new FormControl('', Validators.required),
+      mobileNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.pattern('^[0-9]{11}$'),
+      ]),
       isActive: new FormControl(true),
-      isAdmin: new FormControl(false)
-    })
+      isAdmin: new FormControl(false),
+    });
   }
 
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
   }
 
-  ngOnInit(): void {
-    this.userId = Number(this.route.snapshot.paramMap.get('id'));
-    if (this.userId) {
-      const blog = this.adminPageService.getUserById(this.userId).subscribe({
-        next: (user: User) => {
-          this.userForm.patchValue({
-              userName: user.userName,
-              firstName: user.firstName,
-              middleName: user.middleName,
-              lastName: user.lastName,
-              birthDate: user.birthDate,
-              interests: user.interests,
-              password: user.password,
-              email: user.email,
-              mobileNumber: user.mobileNumber,
-              isActive: user.isActive,
-              isAdmin: user.isAdmin
-            });
-        }}
-      );
+  private generatePassword(): string {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < this.passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * charactersLength);
+      result += characters.charAt(randomIndex);
     }
+    return result;
   }
-
-  // get interest(): FormArray {
-  //   return this.userForm?.get('interests') as FormArray;
-  // }
-
-  // addInterest(): void {
-  //   this.interestsFormArray?.controls.push(new FormControl(''));
-  // }
-
-  // removeInterest(index: number): void {
-  //   this.interestsFormArray?.removeAt(index);
-  // }
 
   onSubmit(): void {
     if (this.userForm.valid) {
@@ -94,18 +81,18 @@ export class AdminPageFormComponent {
             email: formValue.email,
             mobileNumber: formValue.mobileNumber,
             isActive: formValue.isActive,
-            isAdmin: formValue.isAdmin
+            isAdmin: formValue.isAdmin,
           };
-          
-          const request =  this.adminPageService.addUser(user);
-          
+
+          const request = this.adminPageService.addUser(user);
+
           this.sub = request.subscribe({
             next: () => {
               this.router.navigate(['/admin-page']);
-            }
-          })
-        }
-      })
+            },
+          });
+        },
+      });
     } else {
       this.userForm.markAllAsTouched();
     }
@@ -119,17 +106,15 @@ export class AdminPageFormComponent {
       lastName: '',
       birthDate: '',
       interests: [''],
-      password: '',
+      password: this.generatePassword(),
       email: '',
       mobileNumber: '',
       isActive: true,
-      isAdmin: false
+      isAdmin: false,
     });
   }
 
   onCancel(): void {
     this.router.navigate(['/admin-page']);
   }
- 
 }
-
