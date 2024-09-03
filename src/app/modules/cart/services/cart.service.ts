6ab14081f9283f47a0dd234e4b12ed91a1aa34b1
bpepito <bernadetteotipep@gmail.com>
2024-09-03 11:02:33 +0800

    edit cart css

--- src/app/modules/cart/services/cart.service.ts
+++ src/app/modules/cart/services/cart.service.ts
@@ -1,116 +1,191 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Cart } from '../model/cart';
-import { forkJoin, map, Observable, of, switchMap, tap } from 'rxjs';
+import { map, Observable, of, switchMap, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   private serverUrl = 'http://localhost:3000/carts';
-  userId: string = "2";
-  status: string =  "added";
+  // userId: string = '1';
+  status: string = 'added';
 
   constructor(private http: HttpClient) {}
 
-  getCarts(): Observable<Cart[]>{
-    if (this.userId === null || this.userId === "0") {
+  private getCurrentUser = () => {
+    let session = localStorage.getItem('session');
+    if (session) {
+      return JSON.parse(session);
+    }
+    return null;
+  };
+
+  private getCurrentUserId = () => {
+    const user = this.getCurrentUser();
+    if (user && user.id) {
+      return user.id;
+    }
+    return null;
+  };
+
+  // with getCurrentUserId
+  getCarts(): Observable<Cart[]> {
+    const currentUserId = this.getCurrentUserId();
+    if (currentUserId === null || currentUserId === '0') {
       console.log('Cart is empty');
       return of([]);
     }
 
-    return this.http.get<Cart[]>(this.serverUrl)
-    .pipe(
-      map(carts => carts.filter(
-        cart => (this.userId ? cart.userId === this.userId : true) &&
-        (this.status ? cart.status === this.status : true )
-      )),
-      tap(filteredCarts => console.log('Fetched and filtered arts:', filteredCarts))
-    )
-  };
-
-  addItemToCart(cart: Cart): Observable<Cart> {
-    return this.http.post<Cart>(this.serverUrl, cart).pipe(
-      tap((newItem: Cart) => console.log('Adding item to cart ', newItem))
-    )
-  }
-
-  deleteItemToCart(id: string): Observable<Cart>  {
-    const url = `${this.serverUrl}/${id}`;
-    return this.http.delete<Cart>(url).pipe(
-      tap(() => console.log(`Deleted cart ${id}`))
+    return this.http.get<Cart[]>(this.serverUrl).pipe(
+      map((carts) =>
+        carts.filter(
+          (cart) =>
+            cart.userId === currentUserId &&
+            (this.status ? cart.status === this.status : true)
+        )
+      ),
+      tap((filteredCarts) =>
+        console.log('Fetched and filtered arts:', filteredCarts)
+      )
     );
   }
 
-  incrementCart(id: string): Observable<Cart> {
+  // hard-code userId
+  // getCarts(): Observable<Cart[]> {
+  //   if (this.userId === null || this.userId === '0') {
+  //     console.log('Cart is empty');
+  //     return of([]);
+  //   }
+
+  //   return this.http.get<Cart[]>(this.serverUrl).pipe(
+  //     map((carts) =>
+  //       carts.filter(
+  //         (cart) =>
+  //           (this.userId ? cart.userId === this.userId : true) &&
+  //           (this.status ? cart.status === this.status : true)
+  //       )
+  //     ),
+  //     tap((filteredCarts) =>
+  //       console.log('Fetched and filtered arts:', filteredCarts)
+  //     )
+  //   );
+  // }
+
+  addItemToCart(newItem: Cart): Observable<Cart> {
+    return this.getCart().pipe(
+      switchMap((carts) => {
+        const existingCart = carts.find(
+          (cart: Cart) => cart.productId === newItem.productId
+        );
+
+        if (existingCart) {
+          existingCart.quantity += newItem.quantity;
+          existingCart.price =
+            (existingCart.price / existingCart.quantity) *
+            existingCart.quantity;
+          return this.http
+            .put<Cart>(`${this.serverUrl}/${existingCart.id}`, existingCart)
+            .pipe(
+              tap((updatedCart) =>
+                console.log('Updated cart item:', updatedCart)
+              )
+            );
+        } else {
+          return this.http
+            .post<Cart>(this.serverUrl, newItem)
+            .pipe(
+              tap((addedCart) => console.log('Added new cart item:', addedCart))
+            );
+        }
+      })
+    );
+  }
+
+  deleteItemFromCart(id: string): Observable<Cart> {
+    const url = `${this.serverUrl}/${id}`;
+    return this.http
+      .delete<Cart>(url)
+      .pipe(tap(() => console.log(`Deleted cart ${id}`)));
+  }
+
+  incrementCount(id: string): Observable<Cart> {
     const url = `${this.serverUrl}/${id}`;
     return this.http.get<Cart>(url).pipe(
-      map(cart => {
-        cart.price = (cart.price / cart.quantity);
+      map((cart) => {
+        cart.price = cart.price / cart.quantity;
         cart.quantity += 1;
         cart.price *= cart.quantity;
         return cart;
       }),
-      switchMap(updateCart => this.http.patch<Cart>(url, updateCart)),
-      tap((updatedCart => console.log(`Added quantity for cart ${id}`)))
-    )
+      switchMap((updateCart) => this.http.patch<Cart>(url, updateCart)),
+      tap((updatedCart) => console.log(`Added quantity for cart ${id}`))
+    );
   }
 
-  decrementCart(id: string): Observable<Cart> {
+  decrementCount(id: string): Observable<Cart> {
     const url = `${this.serverUrl}/${id}`;
     return this.http.get<Cart>(url).pipe(
-      map(cart => {
-        cart.price = (cart.price / cart.quantity);
+      map((cart) => {
+        cart.price = cart.price / cart.quantity;
         cart.quantity -= 1;
 
-        if(cart.quantity === 0) {
-          return this.deleteItemToCart(id).pipe(
+        if (cart.quantity === 0) {
+          return this.deleteItemFromCart(id).pipe(
             tap(() => console.log('Quantity is zero')),
             map(() => null)
-          )
+          );
         }
         cart.price *= cart.quantity;
-        return this.http.patch<Cart>(url, cart).pipe(
-          tap(updatedCart => console.log(`Decrement quantity for cart ${id}`))
-        );
+        return this.http
+          .patch<Cart>(url, cart)
+          .pipe(
+            tap((updatedCart) =>
+              console.log(`Decrement quantity for cart ${id}`)
+            )
+          );
       }),
-      switchMap(result => result as Observable<Cart>)
+      switchMap((result) => result as Observable<Cart>)
     );
   }
 
   editQuantity(id: string, quantity: number): Observable<Cart> {
     const url = `${this.serverUrl}/${id}`;
     return this.http.get<Cart>(url).pipe(
-      map(cart => {
-        cart.price = (cart.price / cart.quantity);
+      map((cart) => {
+        cart.price = cart.price / cart.quantity;
         cart.quantity = quantity;
 
-        if(cart.quantity === 0) {
-          return this.deleteItemToCart(id).pipe(
+        if (cart.quantity === 0) {
+          return this.deleteItemFromCart(id).pipe(
             tap(() => console.log('Quantity is zero')),
             map(() => null)
-          )
+          );
         }
         cart.price *= cart.quantity;
-        return this.http.patch<Cart>(url, cart).pipe(
-          tap(updatedCart => console.log(`Edit quantity for cart ${id}`))
-        );
+        return this.http
+          .patch<Cart>(url, cart)
+          .pipe(
+            tap((updatedCart) => console.log(`Edit quantity for cart ${id}`))
+          );
       }),
-      switchMap(result => result as Observable<Cart>)
+      switchMap((result) => result as Observable<Cart>)
     );
   }
 
-   // derek's
-  getCart(){
-    console.log(`[From Cart Service] Getting cart with user id (user.id not implemented yet)...`)
-    return this.http.get<any[]>(`${this.serverUrl}/carts`).pipe(
-      map((carts) => carts.find((cart: Cart) => cart.userId === "1" ))
-    )
+  // derek's
+  getCart() {
+    console.log(
+      `[From Cart Service] Getting cart with user id (user.id not implemented yet)...`
+    );
+    return this.http
+      .get<any[]>(`${this.serverUrl}/carts`)
+      .pipe(map((carts) => carts.find((cart: Cart) => cart.userId === '1')));
   }
 
-  updateCart(updatedCart:Cart){
+  updateCart(updatedCart: Cart) {
     return this.http
-    .put(`${this.serverUrl}/carts/${updatedCart.id}`, updatedCart)
-    .pipe(tap((x)=> console.log('[From Cart Service] updating ', x)))
+      .put(`${this.serverUrl}/carts/${updatedCart.id}`, updatedCart)
+      .pipe(tap((x) => console.log('[From Cart Service] updating ', x)));
   }
 }
--- src/app/modules/user/models/user.ts
+++ src/app/modules/user/models/user.ts
@@ -1,14 +1,14 @@
 export interface User {
-    id?: string,
-    userName: string,
-    email:string,
-    password:string,
-    firstName: string,
-    middleName: string | null,
-    lastName: string,
-    birthDate: string | null,
-    mobileNumber: string,
-    interests: string[] | null[],
-    isActive: boolean,
-    isAdmin: boolean
+  id?: string;
+  userName: string;
+  email: string;
+  password: string;
+  firstName: string;
+  middleName: string | null;
+  lastName: string;
+  birthDate: string | null;
+  mobileNumber: string;
+  interests: string[] | [];
+  isActive: boolean;
+  isAdmin: boolean;
 }
