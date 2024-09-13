fc70151d3ece5d88a3ac85aab90046eb3831377f
Roxane <rreyes@77global.biz>
2024-09-13 11:58:27 +0800

    removing test routes

--- src/app/modules/transactions/transaction-routing.module.ts
+++ src/app/modules/transactions/transaction-routing.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PendingTransactionComponent } from './pages/pending-transaction/pending-transaction.component';
 import { RouterModule, Routes } from '@angular/router';
-import { PerTransactionComponent } from './components/per-transaction/per-transaction.component';
 
 const routes: Routes = [{
   path: '',
   component: PendingTransactionComponent
-},
-{
-  path: 'checking',
-  component: PerTransactionComponent
 }
 ];
 
